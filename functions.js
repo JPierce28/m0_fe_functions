f0416843b3c10a4c67a155de910bafd86b12ad89
Josh Pierce <joshua.alan.pierce@gmail.com>
2022-08-04 14:54:15 -0600

    Add self eval

--- functions.js
+++ functions.js
@@ -1,14 +1,41 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
+function printGreeting (){
+  console.log(`Hello! How are you today?`);
+
+}
+printGreeting()
+printGreeting()
+printGreeting()
 
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
+function favoriteFood(foodType){
+  console.log(`My favorite food is ${foodType}`);
 
-
+}
+favoriteFood(pizza);
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
+function payRange(maxPay, minPay, companyName){
+  var salary = minPay + maxPay
+  console.log(`Looking for a delievery driver at ${companyName}, make up to $${salary} per year!`);
+  console.log(`Looking for a delievery driver at ${companyName}, make a minimum pay of $${minPay} per year!`);
+}
 
-
+payRange(11000, 9000, "Amazon");
+payRange(11000, 9000, "Amazon");
 // 4: Write a function that satifies the following interaction pattern:
+function checkStock (quantity, product){
+  if (quantity = 3){
+    console.log(`${product} - running low`);
+  }
+  if (quantity >= 4){
+    console.log(`${product} is stocked`)
+  }
+  if (quantity === 0){
+    console.log(`${product} - OUT of stock!`)
+  }
 
+}
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
 
--- selfEval.md
+++ selfEval.md
@@ -1,6 +1,10 @@
 # Self Evaluation
 
 - Reflect on your time management and awareness of where you spend time. Is this a strength or area you are working on? What support do you need in this area? And/or, what tips do you have to share with your classmates in this area?
+  - My time management with this particular lesson was very improved from previous lessons. If I got stuck on a practice example I would just move on and try some other ones. Doing those helped me think of the problem I was stuck on before or add new information to help me solve the previous problem.
 - How would you explain to a 5 year old what a function is?
+  - A function is a grouping of code lines that can provide instructions for you to follow.
 - Share your understanding of arguments and parameters.
+  - An argument is similar to an array in that it contains different values that you can call on to print what you need. Parameters are used to call and assign the arguments you wish to print.
 - What questions/topics (related to Functions) do you want to being to your Small Group?
+  - I will ask similar questions to these. I will ask their understanding of the new vocabulary and what a function could be used for.
